e33e7dab554fcd58c70882c69e0a73e380fcef60
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-06-02 03:58:10 +0300

    Add: the Neccessary computation and display on the basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+subtr = number1 - number2
+mult = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {subtr}")
+print(f"Multiplication of {number1} and {number2} is {mult}")
+
+
