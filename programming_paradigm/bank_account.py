98a2dd6851f2dc67f47b247a007c043b78d84f9d
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-06-15 20:54:12 +0300

    Week 6 Project 0

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self,Balance = 0):
+        self.Balance = Balance
+    
+    def deposit(self,amount):
+        self.Balance += amount
+    
+    def withdraw(self,amount):
+        if (self.Balance - amount) >= 0:
+            self.Balance -= amount
+            return True
+        
+        return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.Balance}")
